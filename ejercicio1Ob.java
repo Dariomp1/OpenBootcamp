0aeefb8a053f8af6903fba2c8eccf01b2740e541
Dariomp1 <119634367+Dariomp1@users.noreply.github.com>
2023-01-15 13:36:21 +0100

    Add files via upload

---
+++ ejercicio1Ob.java
@@ -0,0 +1,26 @@
+package com.example.ejerciciosob;
+
+public class ejercicio1Ob{
+    public class datos{
+        byte byte1=1;
+        int int1=2342;
+        long long1=1287947632L;
+        double double1=123.234;
+        String nombre= Pepe();
+        boolean bl1;
+    }
+    private static String bl1(){
+        return "false";
+    }
+    private static String Pepe() {
+        return "Pepe";
+    }
+    public static void main (String[]args) {
+        System.out.println("Esto es un byte: " + byte.class);
+        System.out.println("Esto es un int: " + int.class);
+        System.out.println("Esto es un long: " + long.class);
+        System.out.println("Esto es un double: " + double.class);
+        System.out.println("Esto es un String con un nombre: "+Pepe());
+        System.out.println("Esto es un booleano: "+bl1());
+    }
+}
